9487fc1ea20a27a8f13974669322aba01547f4c9
shivraj11 <shivrajdeopa11@gmail.com>
2019-09-30 23:39:15 +0530

    first

---
+++ app/libs/redisLib.js
@@ -0,0 +1,77 @@
+//redis lib
+const check = require("./checkLib.js");
+const redis = require('redis');
+let client = redis.createClient({
+
+    port: 12894,
+    host: 'redis-12894.c12.us-east-1-4.ec2.cloud.redislabs.com',
+    password: '6tzBkMSHIPxFGD997Z0174PDvC3KB5b9'
+});
+
+client.on('connect', () => {
+
+    console.log("Redis connection successfully opened");
+
+});
+
+let getAllUsersInAHash = (hashName, callback) => {
+
+    client.HGETALL(hashName, (err, result) => {
+        
+
+        if (err) {
+
+            console.log(err);
+            callback(err, null)
+
+        } else if (check.isEmpty(result)) {
+
+            console.log("online user list is empty");
+            console.log(result)
+
+            callback(null, {})
+
+        } else {
+
+            console.log(result);
+            callback(null, result)
+
+        }
+    });
+
+
+}// end get all users in a hash
+
+// function to set new online user.
+let setANewOnlineUserInHash = (hashName, key, value, callback) => {
+   
+    client.HMSET(hashName, [
+        key, value
+    ], (err, result) => {
+        if (err) {
+            console.log(err);
+            callback(err, null)
+        } else {
+
+            console.log("user has been set in the hash map");
+            console.log(result)
+            callback(null, result)
+        }
+    });
+
+
+}// end set a new online user in hash
+
+let deleteUserFromHash = (hashName,key)=>{
+
+    client.HDEL(hashName,key);
+    return true;
+
+}// end delete user from hash
+
+module.exports = {
+    getAllUsersInAHash:getAllUsersInAHash,
+    setANewOnlineUserInHash:setANewOnlineUserInHash,
+    deleteUserFromHash:deleteUserFromHash
+}
+
